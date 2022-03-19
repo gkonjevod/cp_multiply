4efe4ec8c65ffb61e6126f5107f654148a9783fc
Goran Konjevod <goran.konjevod@gmail.com>
2022-03-18 22:00:15 -0700

    Cleaned up box packing example.

--- cp_multiply/examples/make_box_packing_cp.py
+++ cp_multiply/examples/make_box_packing_cp.py
@@ -11,11 +11,6 @@ from ..cp_utils import to_degrees, dihedral_angle, normal
 
 from math import sqrt, pi, tan, atan2
 
-# calculate the non-integer coordinate
-twentytwoandhalf = 22.5 * pi / 180
-CD = 2*sqrt(2) * tan (twentytwoandhalf)
-C_x = 4 - CD / sqrt(2)
-
 box_packing_cell_nodes = {'A': (2, 0), 
                        'B': (4, 0), 
                        'C': (2, 2), 
@@ -67,23 +62,4 @@ def generate_box_packing():
     grid = c1.make_grid(grid_size = (5, 5), overlap_frac = 0.25)
     grid.save_cp('box_packing_5x5')
 
-    # l5 = ((16, 0), (16, 1))
-    # l6 = ((0, 16), (1, 16))
-    # c2 = c1.add_reflection(l5).add_reflection(l6)
-    # c2.save_cp('box_grid_cell_2x2')
-    
-    # l7 = ((32, 0), (32, 1))
-    # l8 = ((0, 32), (1, 32))
-    # c3 = c2.add_reflection(l7).add_reflection(l8)
-    # c3.save_cp('box_grid_cell_4x4')
-
-    # l9 = ((64, 0), (64, 1))
-    # l10 = ((0, 64), (1, 64))
-    # c4 = c3.add_reflection(l9).add_reflection(l10)
-    # c4.save_cp('box_grid_cell_8x8')
-
-    # l11 = ((128, 0), (128, 1))
-    # l12 = ((0, 128), (1, 128))
-    # c5 = c4.add_reflection(l11).add_reflection(l12)
-    # c5.save_cp('box_grid_cell_16x16')
 
